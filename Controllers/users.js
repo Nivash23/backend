32410e1a8853dc05ebae9c7470ec485e4c0aca71
Nivash23 <nivashsubash32@gmail.com>
2023-11-26 09:04:47 +0530

    usercreate application

--- Controllers/users.js
+++ Controllers/users.js
@@ -1,6 +1,7 @@
 const userRouter = require('express').Router();
 const  User = require('../Models/users');
 const bcrypt = require('bcrypt');
+const { err } = require('../utils/logger');
 
 
 userRouter.post('/', async (req, res) => {
@@ -11,7 +12,17 @@ userRouter.post('/', async (req, res) => {
         name,
         passwordHash,
     });
+
     const saveduser = await user.save();
     res.json(saveduser);
+});
+userRouter.get('/', async (req, res) => {
+    User.find({}, {})
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            console.log('there is no data found in db..');
+    })
 })
 module.exports = userRouter;
